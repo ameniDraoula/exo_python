e59be3ce69868e3a88aa58a11b0d06e8280bd092
ameniDraoula <125572570+ameniDraoula@users.noreply.github.com>
2023-02-16 15:29:26 +0100

    Add files via upload

---
+++ exo_python/exo1_test_python.py
@@ -0,0 +1,16 @@
+
+#creation de 20 variable different 
+#collection de varible
+nbr = 5
+x= 6
+somme=nbr+x
+s = 'valeur = ' + x
+float(3) # convertit l'int en float.
+int(3.0) # convertit le float en int.
+int(3.6) # convertit le float en int, en donnant ici 3.
+ch="bonjour"# chaine de caractere
+bool=x<nbr # vaiable boolean
+tuple = (4,5) # creation d'un tuple
+tuple2 = ("un",1,"deux",2)   # création d'un tuple composé de 2 chaînes de caractères c'est un collection de variable de type chaine de caractére
+
+liste = [4,5]               # création d'une liste composée de deux entiers
